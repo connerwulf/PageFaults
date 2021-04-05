1fb758b9400331e187542929ea10794d5e3fe17d
Conner Wulf <connerwulf@usf.edu>
2021-04-05 18:33:00 -0400

    Fixed FIFO and added broken LRU

--- PageFault_ConnerWulf.c
+++ PageFault_ConnerWulf.c
@@ -12,16 +12,20 @@ Author: Conner Wulf
 typedef struct Node
 {
 	struct Node *ptr;
+	struct Node *ptr_fwd;
 	int Page_Num;
+	int FrameNum;
 } Node;
 
 
 //Function definitions
 int search_queue(Node *head, int pNum);
-void print_queue(Node *head);
+void print_queue(Node *head, int length);
 int dequeue(Node **head);
-void enqueue(Node **head, int pNum);
-
+void enqueue(Node **head, int pNum, int frameNum);
+void push(Node **head, Node **tail, int pNum, int frameNum);
+int removeLRU(Node **tail);
+int recentlyUsed(Node **head, int value);
 
 
 /****************************************************************
@@ -29,38 +33,53 @@ void enqueue(Node **head, int pNum);
 ****************************************************************/
 int main(int argc, char *argv[])
 {
-	Node *head = NULL;
-	FILE *file;
-	int pageNumber;
-	int numOfPFs = 0;
-	int usedFrames = 0;
+
 	printf("%s %s %s\n", argv[1],  argv[2],  argv[3]);
+
 	int isFIFO = strcmp(argv[1], "FIFO");
+	int isLRU = strcmp(argv[1], "LRU");
+	int maxFrames = atoi(argv[3]);
+
 	if(isFIFO == 0)
 	{
-		printf("It does");
+		Node *head = NULL;
+		FILE *file;
+		int pageNumber;
+		int numOfPFs = 0;
+		int usedFrames = 0;
+		
+
+		
+
 		if(!(file = fopen(argv[2], "rt")))
 		{
 			perror("Problem reading in file\n");
 			exit(1);
 		}
 
-		while((pageNumber = fgetc(file)) != EOF)
+		fscanf(file, "%d", &pageNumber);
+		while(!feof(file))
 		{
-			printf("%c\n", pageNumber);
-			if(pageNumber != ' ')
+			if(pageNumber >= 0)
 			{
 				if(head == NULL)
 				{
-					enqueue(&head, pageNumber);
+					enqueue(&head, pageNumber, usedFrames);
+					//printf("enqueue page %d\n", pageNumber);
 					usedFrames++;
 					numOfPFs++;
 				}
-				else if(usedFrames < atoi(argv[3]))
+				else if(usedFrames < maxFrames)
 				{
-					enqueue(&head, pageNumber);
-					usedFrames++;
-					numOfPFs++;
+					int check = search_queue(head, pageNumber);
+					if( check != 1)
+					{
+
+						enqueue(&head, pageNumber, usedFrames);
+						//printf("enqueue page %d\n", pageNumber);
+						usedFrames++;
+						numOfPFs++;
+					}
 				}
 				else
 				{
@@ -72,18 +91,107 @@ int main(int argc, char *argv[])
 					else
 					{
 						int temp = dequeue(&head);
-						enqueue(&head, pageNumber);
+						//printf("denqueue frame %d\n", temp);
+						enqueue(&head, pageNumber, temp);
+						//printf("enqueue page %d\n", pageNumber);
+						
 
 						numOfPFs++;
 					}
 				}
 
 			}
+			fscanf(file, "%d", &pageNumber);
 		}
+		fclose(file);
 
 
 		printf("Page Faults: %d\n", numOfPFs);
-		print_queue(head);
+		print_queue(head, maxFrames);
+	}
+
+	else if(isLRU == 0)
+	{
+		Node *head = NULL;
+		Node *tail = NULL;
+
+		// push(&head, &tail, 1, 1);
+		// push(&head, &tail, 2, 2);
+		// push(&head, &tail, 3, 3);
+		// push(&head, &tail, 4, 4);
+		// recentlyUsed(&head, 2);
+		// printf("popped %d\n",removeLRU(&tail));
+		// printf("popped %d\n",removeLRU(&tail));
+		// printf("popped %d\n",removeLRU(&tail));
+		// printf("popped %d\n",removeLRU(&tail));
+		
+		FILE *file;
+		int pageNumber;
+		int numOfPFs = 0;
+		int usedFrames = 0;
+
+
+
+		if(!(file = fopen(argv[2], "rt")))
+		{
+			perror("Problem reading in file\n");
+			exit(1);
+		}
+
+		fscanf(file, "%d", &pageNumber);
+		while(!feof(file))
+		{
+			if(pageNumber >= 0)
+			{
+				if(head == NULL)
+				{
+					push(&head, &tail, pageNumber, usedFrames);
+					printf("enqueue page %d\n", pageNumber);
+					usedFrames++;
+					numOfPFs++;
+				}
+				else if(usedFrames < maxFrames)
+				{
+					int check = recentlyUsed(&head, pageNumber);
+					if( check != 1)
+					{
+
+						push(&head, &tail, pageNumber, usedFrames);
+						printf("enqueue page %d\n", pageNumber);
+						usedFrames++;
+						numOfPFs++;
+					}
+					
+				}
+				else
+				{
+					print_queue(head, maxFrames);
+					printf("LRU Page = %d\n", tail->Page_Num);
+					int noFault = recentlyUsed(&head, pageNumber);
+					if(noFault == 1)
+					{
+						printf("no fault\n");
+					}
+					else
+					{
+						int temp = removeLRU(&tail);
+						printf("dequeue frame %d\n", temp);
+						push(&head, &tail, pageNumber, temp);
+						printf("enqueue page %d\n", pageNumber);
+						numOfPFs++;
+					}
+				}
+
+			}
+			fscanf(file, "%d", &pageNumber);
+			
+		}
+		fclose(file);
+
+
+		printf("Page Faults: %d\n", numOfPFs);
+		print_queue(head, maxFrames);
+
 	}
 }
 
@@ -93,18 +201,30 @@ int main(int argc, char *argv[])
 
 
 //Debug print for custom queue
-void print_queue(Node *head)
+void print_queue(Node *head, int length)
 {
 	Node *temp = head;
 	int num = 0;
-	while (temp != NULL)
+	int pagenumTemp = -1;
+	while(num <= length) 
 	{
-		int pagenumTemp = temp->Page_Num;
-
-		printf("Frame %d has page %c\n",num, pagenumTemp );
-		num++;
-		temp = temp->ptr;
+		if(temp->FrameNum == num)
+		{
+			pagenumTemp = temp->Page_Num;
+			printf("Frame %d has page %d\n",num, pagenumTemp );
+			temp = head;
+			num++;
+		}
+		else if(temp->ptr != NULL)
+		{
+			temp = temp->ptr;
+		}
+		else
+		{
+			num++;
+		}
 	}
+	
 }
 
 //Debug print for custom queue
@@ -144,7 +264,7 @@ int dequeue(Node **head)
 		prev = cur;
 		cur = cur->ptr;
 	}
-	temp = cur->Page_Num;
+	temp = cur->FrameNum;
 	free(cur);
 
 	if(prev)
@@ -158,7 +278,7 @@ int dequeue(Node **head)
 	return temp;
 }
 
-void enqueue(Node **head, int pNum)
+void enqueue(Node **head, int pNum, int frameNum)
 {
 	Node *new = malloc(sizeof(Node));
 
@@ -168,5 +288,109 @@ void enqueue(Node **head, int pNum)
 	}
 	new->Page_Num = pNum;
 	new->ptr = *head;
+	new->FrameNum = frameNum;
 	*head = new;
 }
+
+void push(Node **head, Node **tail, int pNum, int frameNum)
+{
+	Node *new = malloc(sizeof(Node));
+
+	if(!new)
+	{
+		exit(1);
+	}
+
+	new->Page_Num = pNum;
+	new->FrameNum = frameNum;
+
+	if(*tail == NULL && *head == NULL)
+	{
+		new->ptr = *tail;
+		new->ptr_fwd = *head;
+		*head = new;
+		*tail = new;
+	}
+	else
+	{
+		Node *temp = *head;
+		new->ptr = *head;
+		temp->ptr_fwd = new;
+		*head = new;
+
+	}
+}
+
+int removeLRU(Node **tail)
+{
+	if(tail != NULL)
+	{
+		Node *temp = *tail;
+		Node *next = temp->ptr_fwd;
+		*tail = next;
+		free(temp);
+		return temp->FrameNum;
+
+	}
+	else
+	{
+		return 0;
+	}
+}
+int recentlyUsed(Node **head, int value)
+{
+	Node *temp = *head;
+	Node *hold = *head;
+	Node *prev = NULL;
+	Node *next = NULL;
+
+	while (temp != NULL)
+	{
+		int PageTemp = temp->Page_Num;
+		//printf("%d\n", PageTemp);
+		if(PageTemp == value)
+		{
+
+			if(temp != *head)
+			{
+				prev = temp->ptr;
+				next = temp->ptr_fwd;
+				prev->ptr_fwd = next;
+				next->ptr = prev;
+				temp->ptr = *head;
+				hold->ptr_fwd = temp;
+				temp->ptr_fwd = NULL;
+				*head = temp;
+				temp = temp->ptr;
+
+				return 1;
+			}
+			else
+			{
+				temp = temp->ptr;
+			}
+		}
+		else
+		{
+			temp = temp->ptr;
+		}
+		
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
