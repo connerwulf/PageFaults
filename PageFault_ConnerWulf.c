9393f67987dfe0eef70ada5f0f3f2553b650a5bd
Conner Wulf <connerwulf@usf.edu>
2021-04-07 14:52:08 -0400

    Added Summary and annotations to code

--- PageFault_ConnerWulf.c
+++ PageFault_ConnerWulf.c
@@ -5,6 +5,23 @@
 #include <limits.h>
 /*
 Author: Conner Wulf
+
+Approach: 
+	FIFO (Starts Line:)
+		For the FIFO Alogrithm, I used a normal queue since a queue operates in a FIFO fashion.
+		The Queue is built up of nodes that contain a couple pointers, page number, and what frame
+		that page number is in. The FIFO Alogrithm then loops through the input file and adds N Nodes with unique
+		page numbers, to the queue. Where N is the memory size parameter inputed at runtime. When the Framelimit has been 
+		reached, the algorithm will search the queue to see if the page number is already loaded into a frame, if not then 
+		it will simply dequeue then enqueue the next value, Keeping the FIFO state. Frame number was added to the Node struct
+		to aid the print_queue function when printing the queue in order by Frames
+
+	LRU (Starts Line:)
+		This implementation of LRU works very similar to the FIFO alogrithm with one major change. The initial N Frames are filled
+		in the same way as the FIFO Algorithm, However whenever the LRU alogrithm needs to search the queue, it will use the function
+		recentlyUsed() instead of search_queue(). The main difference between these two aglorithms when the recentlyUsed() function finds a match
+		in the queue, it will remove it from its spot in the queue and repoint it to the back of the queue. This ensures that the value at the front of the queue is 
+		always the Least Recently Used Value.
 */
 
 
@@ -19,12 +36,22 @@ typedef struct Node
 
 
 //Function definitions
+//Start and explanation at Line:
 int search_queue(Node *head, int pNum);
+
+//Start and explanation at Line:
 void print_queue(Node *head, int length);
+
+//Start and explanation at Line:
 int dequeue(Node **head);
+
+//Start and explanation at Line:
 void enqueue(Node **head, int pNum, int frameNum);
+
+//Start and explanation at Line:
 void push(Node **head, Node **tail, int pNum, int frameNum);
-int removeLRU(Node **tail);
+
+//Start and explanation at Line:
 int recentlyUsed(Node **head, int value);
 
 
@@ -34,18 +61,26 @@ int recentlyUsed(Node **head, int value);
 int main(int argc, char *argv[])
 {
 
-	printf("\nMemory Size = %s\n", argv[3]);
-
+	//REading in max frames and checking which alogrithm to run
 	int isFIFO = strcmp(argv[1], "FIFO");
 	int isLRU = strcmp(argv[1], "LRU");
 	int maxFrames = atoi(argv[3]);
+
+	//Checking to see if frame size is in bounds provided from assignment
 	if(maxFrames < 1 || maxFrames > 50)
 	{
 		printf("Error: memory size must be between 1 and 50, inclusive\n");
 		exit(1);
 	}
+
+	//printing inputed Memory Frame Size
+	printf("\nMemory Size = %s\n", argv[3]);
+
+
+	/**** START OF FIFO ALGORITHM ****/
 	if(isFIFO == 0)
 	{
+		//Intialize Queue, FILE and variables
 		Node *head = NULL;
 		FILE *file;
 		int pageNumber;
@@ -54,48 +89,69 @@ int main(int argc, char *argv[])
 		
 
 		
-
+		//Open file, throwing error if it cannot open
 		if(!(file = fopen(argv[2], "rt")))
 		{
 			perror("Problem reading in file\n");
 			exit(1);
 		}
 
+		//Scan in first number into variable pageNumber
 		fscanf(file, "%d", &pageNumber);
+
+		//Start looping through the input file
 		while(!feof(file))
 		{
+
+			//Makes sure page number is a non-negative value
+			//Skips if not
 			if(pageNumber >= 0)
 			{
+
+				//Case Where all frames are empty
 				if(head == NULL)
 				{
+					//Add First page number to Frame 0
 					enqueue(&head, pageNumber, usedFrames);
 					//printf("enqueue page %d\n", pageNumber);
 					usedFrames++;
 					numOfPFs++;
 				}
+
+				//Case where some frames are filled but some are still empty
+				//While usedFrames < maxFrames empty frames 1 to frame(argv[3] -1) will be filled
 				else if(usedFrames < maxFrames)
 				{
+					//checks if page exists in another Frame
 					int check = search_queue(head, pageNumber);
 					if( check != 1)
 					{
-
+						//adds value to blank frame if it is not in another frame
 						enqueue(&head, pageNumber, usedFrames);
 						//printf("enqueue page %d\n", pageNumber);
 						usedFrames++;
 						numOfPFs++;
 					}
 				}
+				//Case where a page replacement must occur
 				else
 				{
+
+					//checks if page exists in another Frame
 					int noFault = search_queue(head, pageNumber);
+
 					if(noFault == 1)
 					{
-						// do nothing
+						//No page replacement needed
+						//Skip to next number
 					}
 					else
 					{
+						//Dequeue from front of the queue insures FIFO
 						int temp = dequeue(&head);
 						//printf("denqueue frame %d\n", temp);
+
+						//Enqueue new page number to keep FIFO order
 						enqueue(&head, pageNumber, temp);
 						//printf("enqueue page %d\n", pageNumber);
 						
@@ -105,21 +161,26 @@ int main(int argc, char *argv[])
 				}
 
 			}
+			//Read in the next page number
 			fscanf(file, "%d", &pageNumber);
 		}
+		//close file
 		fclose(file);
 
-
+		//Print out results from FIFO Alogrithm
 		printf("\nThe number of page faults was %d\n", numOfPFs);
 		printf("\nThe final set of frames is\n");
 		print_queue(head, maxFrames);
 	}
 
+
+/*** 	START OF LRU ALGORITHM 	***/
+
 	else if(isLRU == 0)
 	{
+		//Intialize modified Queue, FILE and variables
 		Node *head = NULL;
 		Node *tail = NULL;
-		
 		FILE *file;
 		int pageNumber;
 		int numOfPFs = 0;
@@ -127,30 +188,43 @@ int main(int argc, char *argv[])
 
 
 
+		//Open file, throwing error if it cannot open
 		if(!(file = fopen(argv[2], "rt")))
 		{
 			perror("Problem reading in file\n");
 			exit(1);
 		}
 
+		//Scan in first number into variable pageNumber
 		fscanf(file, "%d", &pageNumber);
+
+		//Start looping through the input file
 		while(!feof(file))
 		{
+			//Makes sure page number is a non-negative value
+			//Skips if not
 			if(pageNumber >= 0)
 			{
+
+				//Case Where all frames are empty
 				if(head == NULL)
 				{
+					//Add First page number to Frame 0
 					push(&head, &tail, pageNumber, usedFrames);
 					//printf("enqueue page %d\n", pageNumber);
 					usedFrames++;
 					numOfPFs++;
 				}
+				//Case where some frames are filled but some are still empty
+				//While usedFrames < maxFrames empty frames 1 to frame(argv[3] -1) will be filled
 				else if(usedFrames < maxFrames)
 				{
+					//checks if page exists in another Frame
+					//If it does it moves the page reference to the end of the queue
 					int check = recentlyUsed(&head, pageNumber);
 					if( check != 1)
 					{
-
+						//adds value to blank frame if it is not in another frame
 						push(&head, &tail, pageNumber, usedFrames);
 						//printf("enqueue page %d\n", pageNumber);
 						usedFrames++;
@@ -160,19 +234,24 @@ int main(int argc, char *argv[])
 				}
 				else
 				{
-					//print_queue(head, maxFrames);
-					//printf("abouttocheck\n");
+					
+					//checks if page exists in another Frame
+					//If it does it moves the page reference to the end of the queue
 					int noFault = recentlyUsed(&head, pageNumber);
-					//printf("noFault = %d on %d\n", noFault, pageNumber);
+
 					if(noFault == 1)
 					{
+						//No page replacement needed
+						//Skip to next number
+
 						//printf("no fault\n");
 					}
 					else
 					{
-
+						//Dequeue value at the front of the queue to get rid of the Least recently used value
 						int temp = dequeue(&head);
 						//printf("dequeue frame %d\n", temp);
+						//add to back of queue
 						push(&head, &tail, pageNumber, temp);
 						//printf("enqueue page %d\n", pageNumber);
 						numOfPFs++;
@@ -180,12 +259,14 @@ int main(int argc, char *argv[])
 				}
 
 			}
+			//Read in next page reference
 			fscanf(file, "%d", &pageNumber);
 			
 		}
+		//close file
 		fclose(file);
 
-
+		//Print Results
 		printf("\nThe number of page faults was %d\n", numOfPFs);
 		printf("\nThe final set of frames is\n");
 		print_queue(head, maxFrames);
@@ -203,12 +284,14 @@ int main(int argc, char *argv[])
 ****************************************************************/
 
 
-//Debug print for custom queue
+//Print N Frames in order from Frame 0 to frame N-1
 void print_queue(Node *head, int length)
 {
 	Node *temp = head;
 	int num = 0;
 	int pagenumTemp = -1;
+
+	//debug to print in order of the queue, not frame
 	// while(temp != NULL) 
 	// {
 	// 	pagenumTemp = temp->Page_Num;
@@ -238,7 +321,8 @@ void print_queue(Node *head, int length)
 	
 }
 
-//Debug print for custom queue
+//Basic queue search, Loops through queue until a match is found or 
+//the end of the queue has been reached
 int search_queue(Node *head, int searchPage)
 {
 	Node *temp = head;
@@ -258,7 +342,7 @@ int search_queue(Node *head, int searchPage)
 	return 0;
 }
 
-
+//Queue dequeue function that returns frame number that was dequeued
 int dequeue(Node **head)
 {
 	Node *cur  = NULL;
@@ -289,6 +373,8 @@ int dequeue(Node **head)
 	return temp;
 }
 
+//Queue Enqueue function that enqueues a page number and sets the
+//frame number based on parameters
 void enqueue(Node **head, int pNum, int frameNum)
 {
 	Node *new = malloc(sizeof(Node));
@@ -303,6 +389,8 @@ void enqueue(Node **head, int pNum, int frameNum)
 	*head = new;
 }
 
+
+//Modified version of enqueue to work with a Tail pointer.
 void push(Node **head, Node **tail, int pNum, int frameNum)
 {
 	Node *new = malloc(sizeof(Node));
@@ -332,22 +420,8 @@ void push(Node **head, Node **tail, int pNum, int frameNum)
 	}
 }
 
-int removeLRU(Node **tail)
-{
-	if(tail != NULL)
-	{
-		Node *temp = *tail;
-		Node *next = temp->ptr_fwd;
-		*tail = next;
-		free(temp);
-		return temp->FrameNum;
-
-	}
-	else
-	{
-		return 0;
-	}
-}
+//Modified from search_queue function. Once match is found on Queue, function
+//reassigns pointers of Queue Node to be at the end of the queue.
 int recentlyUsed(Node **head, int value)
 {
 	Node *temp = *head;
